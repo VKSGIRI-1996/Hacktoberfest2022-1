ad29ae270d3adb6418d98ae455e4d589d0ef13a7
RITESH GIRI <92908246+GIRIRITESH@users.noreply.github.com>
2022-10-20 20:17:30 +0530

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,17 @@
+class Recursion
+{
+	public static void m1(int n)
+	{
+		if(n==0)
+		{	
+			return ;
+		}
+		System.out.println(n);
+		m1(n-1);
+	}
+	public static void main(String args[])
+	{
+		int n=5;
+		m1(n);
+	}
+}	
